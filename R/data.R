db44381173bb5be60567617c93ae9e6abd018202
vjcitn <stvjc@channing.harvard.edu>
2023-09-07 18:02:10 -0400

    make vignette

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' a named vector with values rdfs labels in NCI thesaurus, and names the corresponding formal ontology tags
+#' @docType data
+#' @format named character vector
+#' @examples
+#' data("nci_map", package="ontoProc2")
+#' ncit_map["EFO:1000899"]
+"ncit_map"
