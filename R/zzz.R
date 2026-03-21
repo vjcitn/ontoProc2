5a4073b0dbc347f811c25da4342a48cd69ad7f13
vjcitn <stvjc@channing.harvard.edu>
2026-03-21 19:12:17 -0400

    thinning out vignettes, using S7 demos

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,16 @@
 utils::globalVariables(c("predicate", "subject", "value"))
+
+.onLoad = function(libname, pkgname) {
+S7::method(print, SemsqlConn) <- function(x) {
+  if (!is_connected(x))
+    stop("SemsqlConn object is disconnected; reconnect with semsql_connect()")
+  n_labels <- dbGetQuery(x@con,
+    "SELECT COUNT(*) AS n FROM rdfs_label_statement")$n
+  cat("<SemsqlConn>",
+      " prefix:", x@ontology_prefix,
+      " | labeled terms:", format(n_labels, big.mark = ","),
+      "\n")
+  invisible(x)
+ }
+}
+
