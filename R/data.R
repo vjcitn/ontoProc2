ede32dacb96bfaebd6f133d6c85a79d7371dca24
vjcitn <stvjc@channing.harvard.edu>
2026-03-24 14:55:10 -0400

    identifier maps for CL and phrases

--- R/data.R
+++ R/data.R
@@ -7,3 +7,23 @@
 #' data("ncit_map", package="ontoProc2")
 #' ncit_map["EFO:1000899"]
 "ncit_map"
+
+#' a named vector with mapping from CURIE to cell type phrase for CL.owl of 2025-12-17
+#' @docType data
+#' @format names character vector
+#' @usage
+#' data(tag2cn)
+#' @examples
+#' data("tag2cn", package="ontoProc2")
+#' tag2cn[c("CL:0000000", "CL:0000006")]
+"tag2cn"
+
+#' a named vector with mapping from cell type phrase to CURIE for CL.owl of 2025-12-17
+#' @docType data
+#' @format names character vector
+#' @usage
+#' data(cn2tag)
+#' @examples
+#' data("cn2tag", package="ontoProc2")
+#' cn2tag["Kupffer cell"]
+"cn2tag"
